7fe4cf97f814039e92f1bb3cf712d375a6e704fe
adisak-divemotion <adisak@divemotion.com>
2022-09-02 00:22:39 +0700

    config tailwind

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className="bg-red-500 border-2 border-red-200">Web</h1>
       <Button />
     </div>
   );
