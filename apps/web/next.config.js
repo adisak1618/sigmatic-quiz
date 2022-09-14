34e9909c1011c5ed821c058ca5f9476b90861cc3
adisak-divemotion <adisak@divemotion.com>
2022-09-14 21:51:51 +0700

    add admin secret in config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,8 @@ module.exports = withImages(withTM({
   reactStrictMode: true,
   env: {
     // eslint-disable-next-line turbo/no-undeclared-env-vars
-    GQL_URL: process.env.GQL_URL
+    GQL_URL: process.env.GQL_URL,
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
+    ADMIN_SECRET: process.env.ADMIN_SECRET
   }
 }));
