5de9746675f99530872d2efe477665307bc535f2
adisak-divemotion <adisak@divemotion.com>
2022-09-14 07:27:16 +0700

    update react type

--- packages/ui/Input/input.tsx
+++ packages/ui/Input/input.tsx
@@ -5,12 +5,15 @@ export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
   return (
     <div className="flex w-full gap-6">
       <input
-        type="text"
         className={clsx(
           "h-[44px] w-full rounded-lg border border-gray-4 px-4 outline-none",
           props.disabled && "text-gray-6"
         )}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        onClick={props.onClick}
         {...props}
+        type="text"
       />
     </div>
   );
